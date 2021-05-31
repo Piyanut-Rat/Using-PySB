9c67148f8abccf602338bdada868bd0bb7abcadd
Piyanut-Rat <piyanut.ratpibun@gmail.com>
2021-05-31 21:52:52 +0700

    Update model.py

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 
 import pysb
 from pysb import *
-from pysb.pathfinder.set_path()
+pysb.pathfinder.set_path()
 from pysb.integrate import Solver
 Model() 
 Monomer('A')
@@ -13,3 +13,11 @@ t = [0, 10, 20, 30, 40, 50, 60]
 solver = Solver(model, t)
 solver.run()
 print(solver.y[:, 0])
+
+
+from pysb import *
+
+Model()
+Monomer('A')
+Parameter('k', 3.0)
+Rule('synthesize_A', None >> A(), k)
