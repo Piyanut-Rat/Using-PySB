1ca5cf01c9cf6a153bf1e6157d3140e4d1f08afe
Piyanut-Rat <piyanut.ratpibun@gmail.com>
2021-06-29 21:44:10 +0700

    Create Note_flow.py

---
+++ examples/Note_flow.py
@@ -0,0 +1,10 @@
+'''
+Step files of study
+ 1. first_step.py 
+ 2. reaction_a.py 
+ 3. run_reaction_a.py
+ 4. Hello_ world or Hello_PySB
+## 1. - 4. for understanded the total concept
+## 5. -  for Model creation in depth
+
+'''
