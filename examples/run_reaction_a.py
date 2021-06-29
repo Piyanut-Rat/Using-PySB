422df077cdd984ecefc40769e836df2f17817143
Piyanut-Rat <piyanut.ratpibun@gmail.com>
2021-06-29 21:44:14 +0700

    Create run_reaction_a.py

---
+++ examples/run_reaction_a.py
@@ -0,0 +1,14 @@
+'''
+
+Using a model
+
+'''
+
+from __future__ import print_function
+from pysb.simulator import ScipyOdeSimulator
+from reaction_a import model
+
+t = [0, 10, 20, 30, 40, 50, 60]
+simulator = ScipyOdeSimulator(model, tspan=t)
+simresult = simulator.run()
+print(simresult.species)
